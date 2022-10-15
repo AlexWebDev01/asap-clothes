6aefea04b592be3a5222ea6e897610a281a0fb3c
Alex <alex.udodov.web@gmail.com>
2022-10-15 16:02:45 +0300

    added redux-thunks for categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,24 +4,16 @@ import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 
 import './shop.styles.scss';
-
 
 const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments('categories');
-            dispatch(setCategories(categoriesArray));
-        }
-
-        getCategoriesMap();
+        dispatch(fetchCategoriesAsync());
     }, []);
-    
 
     return (
         <Routes>
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import persistStore  from 'redux-persist/es/persistStore';
 import persistReducer from 'redux-persist/es/persistReducer';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { legacy_createStore as createStore } from "redux"
 
@@ -17,10 +18,12 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(
     Boolean
     ); //the same logger that we made is in middleware folder
-        //if I don't need redux console.logs, just change 'production' to 'development'
+       //if I don't need redux console.logs, just change 'production' to 'development'
+
+    
 
 //Allow Redux devtool Chrome extension. If there is no Redux devtools - uses standart compose.
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
