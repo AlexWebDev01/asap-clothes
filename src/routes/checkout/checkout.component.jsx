06a9a93446b5d4023f829eca8900f6a4dc2265a7
Alex <alex.udodov.web@gmail.com>
2022-10-17 14:18:29 +0300

    added paymentform with stripe

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import './checkout.styles.scss';
 
@@ -33,6 +34,7 @@ const Checkout = () => {
                 <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
             ))}
             <span className='total'>Total: ${cartTotal}</span>
+            <PaymentForm />
         </div>
     );
 };
