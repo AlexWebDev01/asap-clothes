2b06078c43132262272ffa0efc5a5c82b3006865
Alex <alex.udodov.web@gmail.com>
2022-10-18 15:47:27 +0300

    typed store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,5 @@
-import { compose, applyMiddleware } from 'redux';
-import persistStore  from 'redux-persist/es/persistStore';
-import persistReducer from 'redux-persist/es/persistReducer';
+import { compose, applyMiddleware, Middleware } from 'redux';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import createSagaMiddleware from '@redux-saga/core';
@@ -11,8 +10,19 @@ import { legacy_createStore as createStore } from "redux"
 
 import { rootReducer } from './root-reducer';
 
+export type RootState = ReturnType<typeof rootReducer>
 
-const persistConfig = {
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+    }
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+    whitelist: (keyof RootState)[]
+}
+
+const persistConfig: ExtendedPersistConfig = {
     key: 'root',
     storage, //= storage: storage,
     whitelist: ['cart'] //string with types that you want to persist (in rootReducer const)
@@ -23,10 +33,9 @@ const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger, sagaMiddleware].filter(Boolean); 
-
-
-    
+const middleWares = [
+    process.env.NODE_ENV !== 'production' && logger, sagaMiddleware
+    ].filter((middleware): middleware is Middleware => Boolean(middleware)); 
 
 //Allow Redux devtool Chrome extension. If there is no Redux devtools - uses standart compose.
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
