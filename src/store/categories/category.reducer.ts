084ed417b27e2cbf699d1892ffe85c798ad29fa5
Alex <alex.udodov.web@gmail.com>
2022-10-18 14:22:33 +0300

    typed cart action, types, selector and reducer

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -17,7 +17,7 @@ export const CATEGORIES_INITIAL_STATE = {
 
 export const categoriesReducer = (
     state = CATEGORIES_INITIAL_STATE, 
-    action = {} as AnyAction
+    action: AnyAction
     ): CategoriesState => {
         if(fetchCategoriesStart.match(action)) {
             return {...state, isLoading: true };
