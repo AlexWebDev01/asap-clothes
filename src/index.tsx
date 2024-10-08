0bc3b10c9a61e8aed3eea166e2b5601a53a9c217
Alex <alex.udodov.web@gmail.com>
2024-10-08 21:29:37 +0300

    refactor: removed unused package

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { stripePromise } from './utils/stripe/stripe.utils';
 
 import App from './App';
-
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -27,8 +25,3 @@ root.render(
     </PersistGate>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
